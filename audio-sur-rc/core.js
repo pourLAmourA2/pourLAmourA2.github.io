573df349028ed5525198579d2d962568099fda8d
Pi_Aire-LU_c <testeurAMOur@hotmail.com>
2015-12-01 22:36:46 -0500

    Indentations - Code audio dans core.js

--- audio-sur-rc/core.js
+++ audio-sur-rc/core.js
@@ -1,48 +1,55 @@
-var boPlay = document.getElementById("boPlay");
-boPlay.disabled = true;
+var DEBUG = false;
 
 var mainCanvas = document.getElementById("mainCanvas");
 var mainContex = mainCanvas.getContext('2d');
 var mainWidth = mainCanvas.width;
 var mainHeight = mainCanvas.height;
 
+var boPlay = document.getElementById("boPlay");
+boPlay.disabled = true;
+
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var scaledSampleRate = 8192;
+
 
 var onStartPlay = function(boGetSource) {
-  boGetSource.disabled = true;
-  boPlay.disabled = true;
+    boGetSource.disabled = true;
+    boPlay.disabled = true;
 
-  //audioSamples = decodeAudio(mainContex, );
+    var audixel = new Audixel();
+    var audioSamples = audixel.decodeAudio(mainContex, mainWidth, scaledSampleRate, audioCtx.sampleRate);
 
-  // audio.play(
+    // audio.play(
 
-  /*audioCtx.resume().then(function() {
-    bufferSource = audioCtx.createBufferSource();
-    bufferSource.buffer = recSamples;
-    bufferSource.connect(audioCtx.destination);
-    bufferSource.onended = function() { onStopPlay(boGetSource); }
-    bufferSource.start();
+    /*audioCtx.resume().then(function() {
+        bufferSource = audioCtx.createBufferSource();
+        bufferSource.buffer = recSamples;
+        bufferSource.connect(audioCtx.destination);
+        bufferSource.onended = function() { onStopPlay(boGetSource); }
+        bufferSource.start();
 
-    boPlay.onclick = function() { bufferSource.stop(); }
+        boPlay.onclick = function() { bufferSource.stop(); }
+        boPlay.disabled = false;
+    });*/
+
+    boPlay.onclick = function() { onStopPlay(boGetSource); }
     boPlay.disabled = false;
-  });*/
-
-  boPlay.onclick = function() { onStopPlay(boGetSource); }
-  boPlay.disabled = false;
 }
 
 
 var onStopPlay = function(boGetSource) {
-  boPlay.disabled = true;
+    boPlay.disabled = true;
 
-  /*audioCtx.suspend().then(function() {
+    /*audioCtx.suspend().then(function() {
+        boGetSource.disabled = false;
+        boPlay.onclick = function() { onStartPlay(boGetSource); }
+        boPlay.disabled = false;
+    });*/
+
     boGetSource.disabled = false;
     boPlay.onclick = function() { onStartPlay(boGetSource); }
     boPlay.disabled = false;
-  });*/
-
-  boGetSource.disabled = false;
-  boPlay.onclick = function() { onStartPlay(boGetSource); }
-  boPlay.disabled = false;
 }
 
 
