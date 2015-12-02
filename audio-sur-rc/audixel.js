6da5873356097e16ee36a16687502f3ac16bcc54
Pi_Aire-LU_c <testeurAMOur@hotmail.com>
2015-12-01 20:35:32 -0500

    Implémentation de Audixel - décodage complet

--- audio-sur-rc/audixel.js
+++ audio-sur-rc/audixel.js
@@ -1,7 +1,39 @@
-
+// Interface between Pixels and audio data
 
 function Audixel() {
-  this.audioPath = [0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+    this.audioPath = [0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+    this.center = 128.0;
+    this.amp = 127.0;
+
+    this.indexToCoord = function(index) {
+        var coord = [0, 0];
+        var mask = [1, 1];
+
+        for (var p = 0; p < this.audioPath.length; p++) {
+            coord[this.audioPath[p]] |= (index & 1) * mask[this.audioPath[p]];
+            index >>= 1; mask[this.audioPath[p]] <<= 1;
+        }
+
+        return {x:coord[0], y:coord[1]};
+    }
+
+    this.decodeAudio = function(cvCtx, cvWidth, lowRate, highRate) {
+        var audioPixels = canvasCtx.getImageData(0, cvWidth, cvWidth, cvWidth).data;
+        var nbScaledSamples = cvWidth * cvWidth;
+        var scaledSamples = new Float32Array(nbScaledSamples);
+
+        // Basic decoding: Y=R=G=B
+        for (var s = 0; s < nbScaledSamples; s++) {
+            var coord = this.indexToCoord(s);
+            scaledSamples[s] = (audioPixels[(coord.y * cvWidth + coord.x) * 4 + 1] - this.center) / this.amp;
+        }
+
+        var dct = new Dct();
+        dct.decode(scaledSamples, nbScaledSamples);
+
+        var scaler = new Scaler(lowRate, highRate);
+        return scaler.upscale(scaledSamples, nbScaledSamples);
+    }
 }
 
 
